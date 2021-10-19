e4e1e0c541e42f606738c1c9779e7a37a3d3bc94
CarlosCatalan <carloscatalan@CarlosCatalan-MacBook-Pro.local>
2021-10-19 11:12:39 -0700

    Format

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,14 +6,14 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
- 
+
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
   features {}
 }
- 
+
 data "azurerm_client_config" "current" {}
 
 
