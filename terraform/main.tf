c0f59eb226df55649683e8d3b2687e8dddc5d273
CarlosCatalan <carloscatalan@CarlosCatalan-MacBook-Pro.local>
2021-10-19 11:19:23 -0700

    New Version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,3 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "carlos-catalan"
-    storage_account_name = "carloscatalanl"
-    container_name       = "terraform-state-ci-proof"
-    key                  = "terraform.tfstate"
-  }
-}
-
-provider "azurerm" {
-  # The "feature" block is required for AzureRM provider 2.x.
-  # If you're using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
-  features {}
-}
-
-data "azurerm_client_config" "current" {}
-
-
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "my_virtual_network" {
   name                = var.vnet-name
